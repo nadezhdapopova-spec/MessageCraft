c4c3d989ab6fbc4ae5a4d53749f4aa5e3e1477ac
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-11-06 02:53:48 +0700

    modified apps.py in app campaigns

--- campaigns/apps.py
+++ campaigns/apps.py
@@ -3,8 +3,6 @@ import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.apps import AppConfig
 
-from campaigns.services.campaigns_services import send_scheduled_campaigns
-
 
 class CampaignsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -15,6 +13,8 @@ class CampaignsConfig(AppConfig):
         """Запускает планировщик при старте Django. Проверяет рассылки каждую минуту"""
         if os.environ.get("RUN_MAIN") != "true":
             return
+
+        from campaigns.services.campaigns_services import send_scheduled_campaigns
 
         scheduler = BackgroundScheduler(timezone="Europe/Moscow")
         scheduler.add_job(send_scheduled_campaigns, "interval", minutes=1)
--- reports/views.py
+++ reports/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count, Q
-# from campaigns.models import Campaign
+from campaigns.models import Campaign, Attempt
 from clients.models import Client
-# from attempts.models import Attempt
 from django.views.generic import TemplateView, ListView
 
 
@@ -11,18 +10,18 @@ class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "reports/home.html"
 
 
-# def get_context_data(self, **kwargs):
-#     context = super().get_context_data(**kwargs)
-#
-#     context["total_campaigns"] = Campaign.objects.count()
-#     context["active_campaigns"] = Campaign.objects.filter(status="RUNNING").count()
-#     context["unique_clients"] = Client.objects.values("email").distinct().count()
-#
-#     context["total_attempts"] = Attempt.objects.count()
-#     context["successful_attempts"] = Attempt.objects.filter(status="SUCCESS").count()
-#     context["failed_attempts"] = Attempt.objects.filter(status="FAIL").count()
-#
-#     return context
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+
+    context["total_campaigns"] = Campaign.objects.count()
+    context["active_campaigns"] = Campaign.objects.filter(status="RUNNING").count()
+    context["unique_clients"] = Client.objects.values("email").distinct().count()
+
+    context["total_attempts"] = Attempt.objects.count()
+    context["successful_attempts"] = Attempt.objects.filter(status="SUCCESS").count()
+    context["failed_attempts"] = Attempt.objects.filter(status="FAIL").count()
+
+    return context
 
 
 class CampaignReportView(LoginRequiredMixin, ListView):
@@ -34,27 +33,27 @@ class CampaignReportView(LoginRequiredMixin, ListView):
     login_url = "users:login"
 
 
-    # def get_queryset(self):
-    #     user = self.request.user
-    #     status_filter = self.request.GET.get("status")
-    #
-    #     qs = (Campaign.objects.annotate(total_attempts=Count("attempts"),
-    #                                       success_count=Count("attempts", filter=Q(attempts__status="SUCCESS")),
-    #                                       fail_count=Count("attempts", filter=Q(attempts__status="FAIL")),
-    #                                       )
-    #             .select_related("owner").order_by("-created_at"))
-    #
-    #     if not user.is_staff:
-    #         qs = qs.filter(owner=user)
-    #
-    #     if status_filter:
-    #         qs = qs.filter(status=status_filter)
-    #
-    #     return qs
-    #
-    #
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context["current_status"] = self.request.GET.get("status", "")
-    #     context["statuses"] = Campaign.STATUS_CHOICES
-    #     return context
+    def get_queryset(self):
+        user = self.request.user
+        status_filter = self.request.GET.get("status")
+
+        qs = (Campaign.objects.annotate(total_attempts=Count("attempts"),
+                                          success_count=Count("attempts", filter=Q(attempts__status="SUCCESS")),
+                                          fail_count=Count("attempts", filter=Q(attempts__status="FAIL")),
+                                          )
+                .select_related("owner").order_by("-created_at"))
+
+        if not user.is_staff:
+            qs = qs.filter(owner=user)
+
+        if status_filter:
+            qs = qs.filter(status=status_filter)
+
+        return qs
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["current_status"] = self.request.GET.get("status", "")
+        context["statuses"] = Campaign.STATUS_CHOICES
+        return context
